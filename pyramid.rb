d73680f04e0042d77fe5c3ddac0c489cbd3bcaf3
Brendan O'Handley <brendan.ohandley@gmail.com>
2017-11-30 17:46:58 -0600

    add code for edge cases if num is not an integer and char is not a string

--- pyramid.rb
+++ pyramid.rb
@@ -10,6 +10,8 @@
 #(num-1) spaces before the char
 
 def build_pyramid(num, char)
+  raise "num is not an integer" if num.class != Integer
+  raise "char is not a string" if char.class != String
   pyramid = ""
   
   num.times { |index| pyramid += padding(num, index) + row(char, index) }
@@ -28,3 +30,4 @@ end
 puts build_pyramid(1, "$")  
 puts build_pyramid(4, "*")
 puts build_pyramid(10, "q")
+puts build_pyramid(2, 2)
