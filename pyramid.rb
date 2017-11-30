60feb2eea3e92df025187863a8a7ab2f76172750
Brendan O'Handley <brendan.ohandley@gmail.com>
2017-11-30 17:38:23 -0600

    add single responsibilty methods

--- pyramid.rb
+++ pyramid.rb
@@ -8,3 +8,20 @@
 #print multiple char on each line
 #one space in between each char
 #(num-1) spaces before the char
+
+def build_pyramid(num, char)
+  pyramid = ""
+  
+  num.times { |index| pyramid += padding(num, index) + row(char, index) }
+  
+  pyramid
+end
+
+def padding(num, index)
+  " " * (num - index - 1)
+end
+
+def row(char, index)
+  (char + " ") * (index + 1) + "\n" 
+end
+  
