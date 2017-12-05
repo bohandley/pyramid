543ed8f363936cbc534c23cb926d92da9e4e354a
Brendan O'Handley <brendan.ohandley@gmail.com>
2017-12-05 13:20:58 -0600

    test for returning a string and returning a string with num amount of new lines

--- pyramid_spec.rb
+++ pyramid_spec.rb
@@ -1,11 +1,15 @@
 require_relative('pyramid')
 require 'rspec'
 
-describe 'Fizz-buzz' do
+describe '.pyramid' do
   
   describe 'takes an integer and a string' do
     it 'returns a string' do
-      expect(build_pyramid(1, '$')).to eq '$'
+      expect(build_pyramid(1, '$')).to be_a String
+    end
+
+    it 'returns a string with num amount of new lines' do
+      expect(build_pyramid(3, '#').scan("\n").count).to eq 3 
     end
   end
 
